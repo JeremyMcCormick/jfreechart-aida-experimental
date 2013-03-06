701104c4b06ef6fea1b5c0aa9e98d59a7241b533
Jeremy McCormick <jermccormick@gmail.com>
2013-03-05 17:50:38 -0800

    add 2D histogram style converter

--- src/main/java/hep/aida/jfree/converter/StyleConverterFactory.java
+++ src/main/java/hep/aida/jfree/converter/StyleConverterFactory.java
@@ -3,6 +3,7 @@ package hep.aida.jfree.converter;
 import hep.aida.IBaseHistogram;
 import hep.aida.ICloud2D;
 import hep.aida.IHistogram1D;
+import hep.aida.IHistogram2D;
 
 /**
  * @author Jeremy McCormick <jeremym@slac.stanford.edu>
@@ -11,6 +12,7 @@ import hep.aida.IHistogram1D;
 public class StyleConverterFactory
 {
     private static final Histogram1DStyleConverter histogram1DConverter = new Histogram1DStyleConverter();
+    private static final Histogram2DStyleConverter histogram2DConverter = new Histogram2DStyleConverter();
     private static final Cloud2DStyleConverter cloud2DConverter = new Cloud2DStyleConverter();    
     
     public static StyleConverter getStyleConverter(IBaseHistogram hist) 
@@ -19,6 +21,8 @@ public class StyleConverterFactory
             return histogram1DConverter;
         } else if (hist instanceof ICloud2D) {
             return cloud2DConverter;
+        } else if (hist instanceof IHistogram2D) {
+            return histogram2DConverter;
         } else {
             return null;
         }

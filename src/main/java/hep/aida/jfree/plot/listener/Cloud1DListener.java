efe6c8c14b50666e2ef10809b5fc805eddb374dc
Jeremy McCormick <jermccormick@gmail.com>
2013-03-20 02:41:44 -0700

    add listener for Cloud1D

---
+++ src/main/java/hep/aida/jfree/plot/listener/Cloud1DListener.java
@@ -0,0 +1,30 @@
+package hep.aida.jfree.plot.listener;
+
+import hep.aida.IBaseHistogram;
+import hep.aida.ICloud1D;
+import hep.aida.jfree.converter.Histogram1DConverter;
+
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYDataset;
+
+public class Cloud1DListener extends PlotListener {
+    
+    ICloud1D c1d;
+        
+    Cloud1DListener(IBaseHistogram hist, JFreeChart chart, int[] datasetIndices) {
+        super(hist, chart, datasetIndices);
+        if (!(hist instanceof ICloud1D)) {
+            throw new IllegalArgumentException("hist is not an instance of ICloud1D.");
+        }
+        c1d = (ICloud1D) hist;
+    }
+
+    public synchronized void update() {
+        XYPlot plot = (XYPlot) chart.getPlot();
+        XYDataset[] datasets = Histogram1DConverter.createDatasets(c1d.histogram());
+        for (int i = 0; i < datasetIndices.length; i++) {
+            plot.setDataset(datasetIndices[i], datasets[i]);
+        }
+    }
+}

281be8ea5a3d0a48aa0c9e3fe49d55f71884718f
Jeremy McCormick <jermccormick@gmail.com>
2013-03-22 16:58:28 -0700

    dump output to file rather than screen

---
+++ src/test/java/hep/aida/jfree/PlotterStylePrinterTest.java
@@ -0,0 +1,31 @@
+package hep.aida.jfree;
+
+import hep.aida.jfree.plot.style.util.PlotterStylePrinter;
+import hep.aida.jfree.test.AbstractPlotTest;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+/**
+ * @author Jeremy McCormick <jeremym@slac.stanford.edu>
+ * @version $Id: $
+ */
+public class PlotterStylePrinterTest extends AbstractPlotTest {
+    
+    public void test() {
+        PrintStream ps = null;
+        File outputFile = new File("target/" + this.getClass().getSimpleName() + ".txt");
+        try {
+            ps = new PrintStream(outputFile);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        PlotterStylePrinter printer = new PlotterStylePrinter(style, ps);
+        printer.print();
+        System.out.println("Wrote test output to " + outputFile.getPath());
+    }    
+    
+    public void plot() {
+    }
+}

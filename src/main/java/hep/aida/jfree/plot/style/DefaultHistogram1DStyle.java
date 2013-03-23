37afe53bb8eaf0b416e309ce87035c3713bd72e9
Jeremy McCormick <jermccormick@gmail.com>
2013-03-22 17:18:50 -0700

    few more minor changes; still needs work

--- src/main/java/hep/aida/jfree/plot/style/DefaultHistogram1DStyle.java
+++ src/main/java/hep/aida/jfree/plot/style/DefaultHistogram1DStyle.java
@@ -9,7 +9,6 @@ import hep.aida.ref.plotter.PlotterStyle;
 public class DefaultHistogram1DStyle extends PlotterStyle {
     
     public DefaultHistogram1DStyle() {
-        //initializeBaseStyle();
         applyDefaultSettings();
     }
     
@@ -18,9 +17,16 @@ public class DefaultHistogram1DStyle extends PlotterStyle {
         this.setParameter("allowZeroSuppression", "false");
         this.dataStyle().markerStyle().setVisible(false);
         this.dataStyle().lineStyle().setVisible(false);
-        this.dataStyle().lineStyle().setThickness(4);
+        this.dataStyle().lineStyle().setThickness(2);
         this.dataStyle().fillStyle().setVisible(false);
         this.dataStyle().outlineStyle().setVisible(false);
-        this.dataStyle().fillStyle().setOpacity(0.0);
+        this.dataBoxStyle().setVisible(true);
+        this.dataBoxStyle().borderStyle().setVisible(true);
+        this.dataBoxStyle().borderStyle().setThickness(2);
+        this.dataBoxStyle().borderStyle().setColor("black");
+        this.dataBoxStyle().backgroundStyle().setVisible(true);
+        this.dataBoxStyle().backgroundStyle().setColor("white");
+        this.xAxisStyle().lineStyle().setThickness(4);
+        this.yAxisStyle().lineStyle().setThickness(4);
     }
 }

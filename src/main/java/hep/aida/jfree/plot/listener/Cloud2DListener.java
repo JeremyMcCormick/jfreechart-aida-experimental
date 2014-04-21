bd498e28536ccb1dc60f5ba82805d9752f1666a8
Jeremy McCormick <jermccormick@gmail.com>
2014-04-21 16:28:35 -0700

    Fix 2D cloud listener so it reconfigures the axes if necessary.

--- src/main/java/hep/aida/jfree/plot/listener/Cloud2DListener.java
+++ src/main/java/hep/aida/jfree/plot/listener/Cloud2DListener.java
@@ -1,12 +1,8 @@
 package hep.aida.jfree.plot.listener;
 
-//import hep.aida.IBaseHistogram;
 import hep.aida.ICloud2D;
-import hep.aida.jfree.dataset.DatasetConverter;
 
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.xy.XYSeriesCollection;
 
 /**
  * @author Jeremy McCormick <jeremym@slac.stanford.edu>
@@ -20,13 +16,12 @@ public class Cloud2DListener extends PlotListener<ICloud2D> {
         super(cloud, chart, datasetIndices);
         this.cloud = (ICloud2D) cloud;
     }
-
+    
     public synchronized void update() {
-        //chart.setNotify(false);
-        XYPlot plot = (XYPlot) chart.getPlot();
-        XYSeriesCollection dataset = DatasetConverter.convert(cloud);
-        plot.setDataset(datasetIndices[0], dataset);
-        //chart.setNotify(true);
-        //chart.fireChartChanged();
-     }
+        chart.setNotify(false);        
+        chart.getXYPlot().configureDomainAxes();
+        chart.getXYPlot().configureRangeAxes();
+        chart.setNotify(true);
+        chart.fireChartChanged();
+    }
 }

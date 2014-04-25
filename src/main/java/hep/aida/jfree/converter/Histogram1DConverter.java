1b4341688da06ad96e2ea3064077866a15c328b5
Jeremy McCormick <jermccormick@gmail.com>
2014-04-24 17:02:53 -0700

    Add configuration of axes.

--- src/main/java/hep/aida/jfree/converter/Histogram1DConverter.java
+++ src/main/java/hep/aida/jfree/converter/Histogram1DConverter.java
@@ -17,6 +17,7 @@ import org.jfree.chart.renderer.xy.XYErrorRenderer;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.chart.renderer.xy.XYStepRenderer;
+import org.jfree.data.RangeType;
 import org.jfree.data.xy.XYDataset;
 
 /**
@@ -33,33 +34,36 @@ public class Histogram1DConverter implements Converter<IHistogram1D> {
         return IHistogram1D.class;
     }
 
-    public JFreeChart convert(IHistogram1D h1d, IPlotterStyle style) {
+    public JFreeChart convert(IHistogram1D histogram, IPlotterStyle style) {
         
-        // Create the backing datasets.
-        XYDataset[] datasets = createDatasets(h1d);
+        // Create the backing datasets which are just 
+        // adapters to the AIDA 1D histogram type.
+        XYDataset[] datasets = createDatasets(histogram);
 
-        // Array for the renderers.
+        // Create array for the renderers.
         XYItemRenderer[] renderers = new XYItemRenderer[datasets.length];
 
-        // Bar renderer which draws bar chart.
+        // Add bar renderer which draws bar chart.
         XYBarRenderer barRenderer = new XYBarRenderer();
         barRenderer.setDrawBarOutline(true);
         renderers[VALUES] = barRenderer;
 
-        // Error renderer that draws error bars and caps.
+        // Add error renderer that draws error bars and caps.
         XYErrorRenderer errorRenderer = new XYErrorRenderer();
         errorRenderer.setBaseShapesVisible(false);
         errorRenderer.setSeriesPaint(ERRORS, Color.black);
         renderers[ERRORS] = errorRenderer;
         
-        // Step renderer which draws the histogram step contour.
+        // Add step renderer which draws the histogram step contour.
         XYItemRenderer stepRenderer = new XYStepRenderer();
         renderers[STEPS] = stepRenderer;
         
-        // Line and shape renderer for lines between points and markers.
+        // Add line and shape renderer for lines between points and markers.
         XYLineAndShapeRenderer pointRenderer = new XYLineAndShapeRenderer();
         renderers[POINTS] = pointRenderer;
         
+        // Turn off the renderering of all series.  
+        // These will be activated later in the style code.
         for (int i=0; i<=3; i++) {
             for (int j=0; j<=3; j++) {
                 renderers[i].setSeriesVisible(j, false);
@@ -67,14 +71,24 @@ public class Histogram1DConverter implements Converter<IHistogram1D> {
         }
                         
         // Set the axis labels.
-        String[] labels = ConverterUtil.getAxisLabels(h1d);
+        String[] labels = ConverterUtil.getAxisLabels(histogram);
+        
+        // Configure X axis.
         NumberAxis xAxis = new NumberAxis(labels[0]);
+        xAxis.setLowerBound(histogram.axis().binLowerEdge(0));
+        xAxis.setUpperBound(histogram.axis().binUpperEdge(histogram.axis().bins() - 1));
+        xAxis.setAutoRange(false);
+        
+        // Configure Y axis.
         NumberAxis yAxis = new NumberAxis(labels[1]);
+        yAxis.setAutoRange(true);
+        yAxis.setAutoRangeIncludesZero(true);
+        yAxis.setRangeType(RangeType.POSITIVE);
 
         // Create the plot without any data.
         XYPlot plot = new XYPlot(null, xAxis, yAxis, null);
 
-        // Add datasets and associated renderers to the plot.
+        // Add datasets and their associated renderers to the plot.
         for (int i = 0, n = datasets.length; i < n; i++) {
             plot.setDataset(i, datasets[i]);
             plot.setRenderer(i, renderers[i]);
@@ -84,7 +98,7 @@ public class Histogram1DConverter implements Converter<IHistogram1D> {
         plot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
 
         // Create the chart.
-        JFreeChart chart = new JFreeChart(h1d.title(), JFreeChart.DEFAULT_TITLE_FONT, plot, false);
+        JFreeChart chart = new JFreeChart(histogram.title(), JFreeChart.DEFAULT_TITLE_FONT, plot, false);
 
         // Apply the default theme.
         ChartFactory.getChartTheme().apply(chart);
@@ -92,7 +106,7 @@ public class Histogram1DConverter implements Converter<IHistogram1D> {
         return chart;
     }
 
-    public static XYDataset[] createDatasets(IHistogram1D h1d) {        
+    private static XYDataset[] createDatasets(IHistogram1D h1d) {        
         XYDataset[] datasets = new XYDataset[4];                             
         Histogram1DAdapter adapter = new Histogram1DAdapter(h1d);
         for (int i=0; i<=3; i++) {

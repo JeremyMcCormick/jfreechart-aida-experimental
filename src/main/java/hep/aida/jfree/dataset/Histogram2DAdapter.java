5082656e9ed19bc12a69a8f5edc7d43f1edf6225
Jeremy McCormick <jermccormick@gmail.com>
2014-09-15 13:21:39 -0700

    Improvements to histogram2D adapter.

--- src/main/java/hep/aida/jfree/dataset/Histogram2DAdapter.java
+++ src/main/java/hep/aida/jfree/dataset/Histogram2DAdapter.java
@@ -3,7 +3,7 @@ package hep.aida.jfree.dataset;
 import hep.aida.IHistogram2D;
 
 import org.jfree.data.xy.AbstractXYZDataset;
-import org.jfree.data.xy.XYZDataset;
+import org.jfree.data.xy.IntervalXYZDataset;
 
 /**
  * This adapter implements JFreeChart's XYZDataset interface 
@@ -13,7 +13,7 @@ import org.jfree.data.xy.XYZDataset;
  * 
  * @author Jeremy McCormick <jeremym@slac.stanford.edu>
  */
-public class Histogram2DAdapter extends AbstractXYZDataset implements XYZDataset, HasZBounds  {
+public class Histogram2DAdapter extends AbstractXYZDataset implements IntervalXYZDataset, HasZBounds  {
 
     IHistogram2D histogram;
     Bounds bounds = new Bounds();
@@ -27,7 +27,7 @@ public class Histogram2DAdapter extends AbstractXYZDataset implements XYZDataset
     }
         
     @Override
-    public int getItemCount(int series) {        
+    public int getItemCount(int series) {
         return histogram.xAxis().bins() * histogram.yAxis().bins();
     }
 
@@ -37,6 +37,7 @@ public class Histogram2DAdapter extends AbstractXYZDataset implements XYZDataset
         return histogram.xAxis().binCenter(x);
     }
     
+    @Override
     public double getXValue(int series, int item) {
         return getX(series, item).doubleValue();
     }
@@ -49,15 +50,12 @@ public class Histogram2DAdapter extends AbstractXYZDataset implements XYZDataset
 
     @Override
     public Number getZ(int series, int item) {
-        int x = getX(series, item).intValue();
-        int y = getY(series, item).intValue();
-        int xBin = histogram.coordToIndexX(x);
-        int yBin = histogram.coordToIndexY(y);
-        return histogram.binHeight(xBin, yBin);
+        return histogram.binHeight(getXBin(item), getYBin(item));
     }
 
     @Override
     public int getSeriesCount() {
+        // This dataset may only have one series.
         return 1;
     }
 
@@ -66,6 +64,7 @@ public class Histogram2DAdapter extends AbstractXYZDataset implements XYZDataset
         return histogram.title();
     }    
            
+    @Override
     public Bounds getZBounds(int series) {
         return bounds;
     }
@@ -74,5 +73,44 @@ public class Histogram2DAdapter extends AbstractXYZDataset implements XYZDataset
         if (histogram.entries() != 0)
             bounds.computeZBounds(this, 0);
         return bounds;
+    }
+
+    @Override
+    public Number getStartXValue(int series, int item) {
+        return histogram.xAxis().binLowerEdge(getXBin(item));
+    }
+
+    @Override
+    public Number getEndXValue(int series, int item) {
+        return histogram.xAxis().binUpperEdge(getXBin(item));
+    }
+
+    @Override
+    public Number getStartYValue(int series, int item) {
+        return histogram.yAxis().binLowerEdge(getYBin(item));
+    }
+
+    @Override
+    public Number getEndYValue(int series, int item) {
+        return histogram.yAxis().binUpperEdge(getYBin(item));
+    }
+
+    @Override
+    public Number getStartZValue(int series, int item) {
+        // The Z values always start at zero.
+        return 0;
+    }
+
+    @Override
+    public Number getEndZValue(int series, int item) {
+        return histogram.binHeight(getXBin(item), getYBin(item));
+    }
+    
+    private int getXBin(int item) {
+        return item % histogram.xAxis().bins();
+    }
+    
+    private int getYBin(int item) {
+        return item / histogram.xAxis().bins();
     }
 }

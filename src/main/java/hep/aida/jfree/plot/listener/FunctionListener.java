6414eb2c4a7df9a9c9c5ec3237f8cb35466edb50
Jeremy McCormick <jermccormick@gmail.com>
2014-09-16 18:10:50 -0700

    Add minor FIXME note.

--- src/main/java/hep/aida/jfree/plot/listener/FunctionListener.java
+++ src/main/java/hep/aida/jfree/plot/listener/FunctionListener.java
@@ -4,7 +4,7 @@ import hep.aida.IFunction;
 
 import org.jfree.chart.JFreeChart;
 
-
+// FIXME: This class should be added to PlotListenerFactory.
 public class FunctionListener extends PlotListener<IFunction> {
 
     IFunction function;

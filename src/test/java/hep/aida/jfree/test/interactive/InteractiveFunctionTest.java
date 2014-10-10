4e3b0a48393a9389d0891bf7c8ef087b3c0dde72
Jeremy McCormick <jermccormick@gmail.com>
2014-10-09 21:39:40 -0700

    Add proof of principle that refitting updates the plotted function

--- src/test/java/hep/aida/jfree/test/interactive/InteractiveFunctionTest.java
+++ src/test/java/hep/aida/jfree/test/interactive/InteractiveFunctionTest.java
@@ -11,16 +11,17 @@ import hep.aida.jfree.test.AbstractPlotTest;
 import java.util.Random;
 
 public class InteractiveFunctionTest extends AbstractPlotTest {
-
+    
     public void test() {
 
         setBatchMode(false);
-
+        setWaitTime(100000);
+        
         // Create a simple histogram.
         //IHistogram1D histogram = histogramFactory.createHistogram1D("histogram", 50, 0, 10);
         IHistogram1D histogram = histogramFactory.createHistogram1D("histogram", 100, -6, 6);        
         Random rand = new Random();
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 500; i++) {
             histogram.fill(rand.nextGaussian());
         }
 
@@ -29,10 +30,10 @@ public class InteractiveFunctionTest extends AbstractPlotTest {
         IFunction function = functionFactory.createFunctionByName("Gaussian", "G");
         
         // Create the Fitter and fit the histogram
-        //"leastsquares"
-        //"cleverchisquared"
-        //"binnedmaximumlikelihood"
-        //"chi2"
+        // "leastsquares"
+        // "cleverchisquared"
+        // "binnedmaximumlikelihood"
+        // "chi2"
         IFitter fitter = fitFactory.createFitter("chi2", "jminuit");
         double[] parameters = new double[3];
         parameters[0] = histogram.maxBinHeight();
@@ -40,11 +41,6 @@ public class InteractiveFunctionTest extends AbstractPlotTest {
         parameters[2] = histogram.rms();
         function.setParameters(parameters);
         IFitResult fitResult = fitter.fit(histogram, function);
-        //IFunction fittedFunction = fitResult.fittedFunction();
-        //for (int i = 0, n = histogram.axis().bins(); i < n; i++) {
-        //    double v = fittedFunction.value(new double[] { histogram.axis().binCenter(i) });
-        //    System.out.println("bin[" + i + "] = " + v);
-        //}
 
         // Display the results
         plotter.createRegion();
@@ -53,17 +49,54 @@ public class InteractiveFunctionTest extends AbstractPlotTest {
 
         // Set function style.
         IPlotterStyle functionStyle = plotterFactory.createPlotterStyle();
-        functionStyle.dataStyle().lineStyle().setColor("purple");
+        functionStyle.dataStyle().lineStyle().setColor("red");
         functionStyle.dataStyle().markerStyle().setVisible(true);
         functionStyle.dataStyle().markerStyle().setColor("black");
         functionStyle.dataStyle().markerStyle().setShape("dot");
         functionStyle.dataStyle().markerStyle().setSize(2);
 
         // Plot function.
+        IFunction originalFunction = fitResult.fittedFunction();
         plotter.region(0).plot(fitResult.fittedFunction(), functionStyle);
-
         plotter.show();
+
+        // Perform a bunch of fills.
+        for (int i = 0; i < 10000; i++) {
+            
+            histogram.fill(rand.nextGaussian());                       
+            if (i % 25 == 0) {
+                System.out.println("refitting on fill #" + i);
+                
+                // Perform another fit.
+                IFitResult refitResult = doExampleFit(functionFactory, histogram);
+                
+                // Copy fit results into original function that was plotted.  
+                // This will trigger an update in the plotter region.
+                originalFunction.setParameters(refitResult.fittedFunction().parameters());
+                
+                // Print refitted params.
+                for (String parameterName : originalFunction.parameterNames()) {
+                    System.out.println("  " + parameterName + " = " + originalFunction.parameter(parameterName));
+                }
+            }
+            
+            // Wait a bit.
+            this.pause(1);
+        }
+        
 
         mode();
     }   
+    
+    IFitResult doExampleFit(IFunctionFactory functionFactory, IHistogram1D histogram) {        
+        IFunction function = functionFactory.createFunctionByName("Gaussian", "G");        
+        IFitter fitter = fitFactory.createFitter("chi2", "jminuit");
+        double[] parameters = new double[3];
+        parameters[0] = histogram.maxBinHeight();
+        parameters[1] = histogram.mean();
+        parameters[2] = histogram.rms();
+        function.setParameters(parameters);
+        IFitResult fitResult = fitter.fit(histogram, function);        
+        return fitResult;
+    }
 }

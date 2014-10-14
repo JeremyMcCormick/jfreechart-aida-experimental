6691198dd1a50da081160a6650b397d576cd1f94
Jeremy McCormick <jermccormick@gmail.com>
2014-10-14 14:04:23 -0700

    Add a few new defaults

--- src/main/java/hep/aida/jfree/plotter/style/util/StyleConstants.java
+++ src/main/java/hep/aida/jfree/plotter/style/util/StyleConstants.java
@@ -17,6 +17,9 @@ public final class StyleConstants {
     public static final Color DEFAULT_GRID_COLOR = Color.gray;
     public static final Stroke DEFAULT_STROKE = new BasicStroke(1.0f);
     
+    public static final Color DEFAULT_AXIS_COLOR = Color.black;
+    public static final Color DEFAULT_TITLE_BORDER_COLOR = Color.black;
+    
     public static final String COLOR_MAP = "colorMap";
     public static final String BOX_PLOT = "box";
     public static final String ELLIPSE_PLOT = "ellipse";

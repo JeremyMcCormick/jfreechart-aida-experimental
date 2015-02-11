3da97f906b441cc2642fe50fee42d2685af47f76
Jeremy McCormick <jermccormick@gmail.com>
2015-02-11 13:16:32 -0800

    Rename class.

--- src/main/java/hep/aida/jfree/plotter/style/registry/DefaultStyleStore.java
+++ src/main/java/hep/aida/jfree/plotter/style/registry/DefaultStyleStore.java
@@ -6,33 +6,40 @@ import hep.aida.ref.plotter.PlotterStyle;
 import hep.aida.ref.plotter.style.registry.BaseStyleStore;
 import hep.aida.ref.plotter.style.registry.StyleRegistry;
 
-public class InMemoryCloningStyleStore extends BaseStyleStore {
+public class DefaultStyleStore extends BaseStyleStore {
 
-    private InMemoryCloningStyleStore() {
+    private DefaultStyleStore() {
         super("DefaultStyleStore");
     }
     
     public static void initialize() {             
-        StyleRegistry.getStyleRegistry().addStore(new InMemoryCloningStyleStore());
+        StyleRegistry.getStyleRegistry().addStore(new DefaultStyleStore());
+        DefaultPlotterStyles.registerDefaultStyles();
     }
     
     public void commit() {
         throw new UnsupportedOperationException("The commit method is not implemented.");
     }
     
-    public void addStyle(IPlotterStyle style) {        
+    public void addStyle(IPlotterStyle style) { 
+        System.out.println("DefaultStyleStore.addStyle - " + style.name());
         if (this.getStyle(style.name()) != null)
                 throw new IllegalArgumentException("There is already a style named " + style.name() + " in this store.");
         addStyle(style.name(), style);
     }
     
     public IPlotterStyle getStyle(String styleName) {
-        IPlotterStyle style = super.getStyle(styleName);
         
+        System.out.println("DefaultStyleStore.getStyle - " + styleName);
+        
+        IPlotterStyle style = super.getStyle(styleName);
+                
         // Clone the style instead of just returning it.
         if (style != null) {
+            System.out.println("found style " + style.name());
             return new DefaultPlotterStyle((PlotterStyle)style);
         } else {
+            System.out.println("could not find style!");
             return null;
         }
     }

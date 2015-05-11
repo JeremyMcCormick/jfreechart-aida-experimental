5eb17a28f6f52ed415b538039a46509eef8bac0c
Jeremy McCormick <jermccormick@gmail.com>
2015-05-11 14:36:23 -0700

    Fix up axis ranges for Cloud1D plots.

--- src/main/java/hep/aida/jfree/plotter/listener/Cloud1DListener.java
+++ src/main/java/hep/aida/jfree/plotter/listener/Cloud1DListener.java
@@ -1,6 +1,8 @@
 package hep.aida.jfree.plotter.listener;
 
 import hep.aida.ICloud1D;
+import hep.aida.IHistogram1D;
+import hep.aida.jfree.converter.Histogram1DConverter;
 
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.xy.XYDataset;
@@ -11,9 +13,24 @@ public class Cloud1DListener extends PlotListener<ICloud1D> {
         super(cloud, chart, dataset);
     }
 
-    public void update() {
-        chart.getXYPlot().getDomainAxis().configure();
-        chart.getXYPlot().getRangeAxis().configure();
+    public void update() {                       
+        if (this.plot.isConverted()) {            
+            IHistogram1D histogram = this.plot.histogram();            
+            this.chart.setNotify(false);
+            double minBinSize = histogram.maxBinHeight() + Histogram1DConverter.findMaxError(histogram);
+            if (minBinSize > 0.) {
+                this.chart.getXYPlot().getRangeAxis().setAutoRangeMinimumSize(minBinSize);
+                this.chart.getXYPlot().getRangeAxis().configure();
+            }
+            
+            double xLower = histogram.axis().binLowerEdge(0);
+            double xUpper = histogram.axis().binUpperEdge(histogram.axis().bins() - 1);
+            if (xUpper > xLower) {
+                this.chart.getXYPlot().getDomainAxis().setLowerBound(xLower);
+                this.chart.getXYPlot().getDomainAxis().setUpperBound(xUpper);
+            }
+            this.chart.setNotify(true);
+        }      
         super.update();
     }
 }

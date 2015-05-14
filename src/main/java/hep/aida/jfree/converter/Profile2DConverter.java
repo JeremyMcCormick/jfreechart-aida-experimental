badf2f7c4e644dab12bd215238967ed3aa83cb37
Jeremy McCormick <jermccormick@gmail.com>
2015-05-13 17:49:29 -0700

    add import for moved class

--- src/main/java/hep/aida/jfree/converter/Profile2DConverter.java
+++ src/main/java/hep/aida/jfree/converter/Profile2DConverter.java
@@ -2,16 +2,17 @@ package hep.aida.jfree.converter;
 
 import hep.aida.IPlotterStyle;
 import hep.aida.IProfile2D;
+import hep.aida.jfree.dataset.Profile2DAdapter;
 
 import org.jfree.chart.JFreeChart;
 
-public class Profile2DConverter implements Converter<IProfile2D> {
+final class Profile2DConverter implements Converter<IProfile2D> {
 
     @Override
     public Class<IProfile2D> convertsType() {
         return IProfile2D.class;
     }
-    
+
     public JFreeChart convert(JFreeChart chart, IProfile2D p2D, IPlotterStyle style) {
         return new Histogram2DConverter().convert(chart, new Profile2DAdapter(p2D), style);
     }

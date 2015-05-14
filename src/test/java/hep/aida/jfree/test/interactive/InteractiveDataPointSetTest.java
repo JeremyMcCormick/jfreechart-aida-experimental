2c7391d441aa19c492d498f61628d6e286d4c27f
Jeremy McCormick <jermccormick@gmail.com>
2015-05-13 17:46:02 -0700

    remove unused code and change Y range of points

--- src/test/java/hep/aida/jfree/test/interactive/InteractiveDataPointSetTest.java
+++ src/test/java/hep/aida/jfree/test/interactive/InteractiveDataPointSetTest.java
@@ -10,32 +10,22 @@ import java.util.Random;
  * <p>
  * Based on this AIDA example:
  * <p>
- * <a href="http://aida.freehep.org/doc/v3.3.0/UsersGuide/dataPointSets.jsp#DataPointSetCreateAndFill">DataPointSetCreateAndFill</a>
+ * <a href="http://aida.freehep.org/doc/v3.3.0/UsersGuide/dataPointSets.jsp#DataPointSetCreateAndFill">
+ * DataPointSetCreateAndFill</a>
+ * 
  * @author Jeremy McCormick <jeremym@slac.stanford.edu>
- *
  */
 public class InteractiveDataPointSetTest extends AbstractPlotTest {
-    
+
     public void testDataPointSet() {
-        
+
         this.setBatchMode(false);
         this.setWaitTime(100000);
-        
-        // Create a one dimensional IDataPointSet.
-        IDataPointSet dps1D = 
-                analysisFactory.createDataPointSetFactory(null).create("dps1D", "one dimensional IDataPointSet", 1);
-        
-        // Fill the one dimensional IDataPointSet
-        double[] yVals1D = { 0.32, 0.45, 0.36, 0.29, 0.34 };
-        double[] yErrP1D = { 0.06, 0.07, 0.03, 0.07, 0.04 };
 
-        for (int i = 0; i < yVals1D.length; i++) {
-            dps1D.addPoint();
-            dps1D.point(i).coordinate(0).setValue(yVals1D[i]);
-            dps1D.point(i).coordinate(0).setErrorPlus(yErrP1D[i]);
-            dps1D.point(i).coordinate(0).setErrorMinus(yErrP1D[i]);
-        }
-        
+        // Create a one dimensional IDataPointSet.
+        IDataPointSet dps1D = analysisFactory.createDataPointSetFactory(null).create("dps1D",
+                "one dimensional IDataPointSet", 1);
+
         plotter.createRegion();
         IPlotterStyle style = plotterFactory.createPlotterStyle();
         style.gridStyle().setVisible(false);
@@ -47,16 +37,17 @@ public class InteractiveDataPointSetTest extends AbstractPlotTest {
         style.dataStyle().outlineStyle().setColor("red");
         style.dataStyle().outlineStyle().setLineType("dashed");
         style.dataStyle().outlineStyle().setThickness(2);
+        style.dataStyle().errorBarStyle().setVisible(true);
         plotter.region(0).plot(dps1D, style);
         plotter.show();
-        
+
         Random random = new Random();
-        int nextPointIndex = yVals1D.length;
+        int nextPointIndex = 0;
         for (int i = 0; i < 1000; i++) {
             dps1D.addPoint();
-            dps1D.point(nextPointIndex).coordinate(0).setValue(random.nextDouble());
-            dps1D.point(nextPointIndex).coordinate(0).setErrorPlus(random.nextDouble() / 10.);
-            dps1D.point(nextPointIndex).coordinate(0).setErrorMinus(random.nextDouble() / 10.);
+            dps1D.point(nextPointIndex).coordinate(0).setValue(random.nextDouble() * 1000);
+            dps1D.point(nextPointIndex).coordinate(0).setErrorPlus(random.nextDouble() * 100);
+            dps1D.point(nextPointIndex).coordinate(0).setErrorMinus(random.nextDouble() * 100);
             ++nextPointIndex;
             synchronized (Thread.currentThread()) {
                 try {
@@ -66,7 +57,7 @@ public class InteractiveDataPointSetTest extends AbstractPlotTest {
                 }
             }
         }
-        
+
         mode();
     }
 }

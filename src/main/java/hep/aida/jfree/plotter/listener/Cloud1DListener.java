316aba0fe712b3b18f5b7db39329030ff6116f1d
Jeremy McCormick <jermccormick@gmail.com>
2015-05-13 17:49:47 -0700

    use method for axes configuration

--- src/main/java/hep/aida/jfree/plotter/listener/Cloud1DListener.java
+++ src/main/java/hep/aida/jfree/plotter/listener/Cloud1DListener.java
@@ -13,24 +13,13 @@ public class Cloud1DListener extends PlotListener<ICloud1D> {
         super(cloud, chart, dataset);
     }
 
-    public void update() {                       
-        if (this.plot.isConverted()) {            
-            IHistogram1D histogram = this.plot.histogram();            
+    public void update() {
+        if (this.plot.isConverted()) {
+            IHistogram1D histogram = this.plot.histogram();
             this.chart.setNotify(false);
-            double minBinSize = histogram.maxBinHeight() + Histogram1DConverter.findMaxError(histogram);
-            if (minBinSize > 0.) {
-                this.chart.getXYPlot().getRangeAxis().setAutoRangeMinimumSize(minBinSize);
-                this.chart.getXYPlot().getRangeAxis().configure();
-            }
-            
-            double xLower = histogram.axis().binLowerEdge(0);
-            double xUpper = histogram.axis().binUpperEdge(histogram.axis().bins() - 1);
-            if (xUpper > xLower) {
-                this.chart.getXYPlot().getDomainAxis().setLowerBound(xLower);
-                this.chart.getXYPlot().getDomainAxis().setUpperBound(xUpper);
-            }
+            Histogram1DConverter.configureAxes(this.chart, histogram);
             this.chart.setNotify(true);
-        }      
+        }
         super.update();
     }
 }

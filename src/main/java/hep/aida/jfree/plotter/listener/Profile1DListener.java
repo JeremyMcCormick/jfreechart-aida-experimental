2fa57706f564b0fe2ec7786051a93a7e035abede
Jeremy McCormick <jermccormick@gmail.com>
2015-05-13 17:55:45 -0700

    remove unnecessary implementation of update method

--- src/main/java/hep/aida/jfree/plotter/listener/Profile1DListener.java
+++ src/main/java/hep/aida/jfree/plotter/listener/Profile1DListener.java
@@ -1,36 +1,18 @@
 package hep.aida.jfree.plotter.listener;
 
+import hep.aida.IProfile1D;
+
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.xy.XYDataset;
 
-import hep.aida.IProfile1D;
-
 /**
  * This is a listener to update display of an IProfile1D in JFreeChart.
+ * 
  * @author Jeremy McCormick <jeremym@slac.stanford.edu>
  */
 public class Profile1DListener extends PlotListener<IProfile1D> {
-    
+
     Profile1DListener(IProfile1D p1D, JFreeChart chart, XYDataset dataset) {
         super(p1D, chart, dataset);
-    }
-    
-    @Override
-    public void update() {
-        this.chart.getXYPlot().getRangeAxis().setAutoRangeMinimumSize(this.plot.maxBinHeight() + findMaxError(this.plot));
-        this.chart.getXYPlot().getRangeAxis().configure();
-        super.update();
-    }
-    
-    private double findMaxError(IProfile1D p1D) {
-        double maxBinHeight = p1D.maxBinHeight();
-        double maxError = 0;
-        for (int i = 0; i < p1D.axis().bins(); i++) {
-            if (p1D.binHeight(i) == maxBinHeight) {
-                if (p1D.binError(i) > maxError)
-                    maxError = p1D.binError(i); 
-            }
-        }
-        return maxError;
     }
 }

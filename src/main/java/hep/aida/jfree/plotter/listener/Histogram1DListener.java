8fd7095b2397a761bce77e6b73fc6d7e35b3915c
Jeremy McCormick <jermccormick@gmail.com>
2015-05-13 17:52:37 -0700

    remove unnecessary method implementation

--- src/main/java/hep/aida/jfree/plotter/listener/Histogram1DListener.java
+++ src/main/java/hep/aida/jfree/plotter/listener/Histogram1DListener.java
@@ -1,7 +1,6 @@
 package hep.aida.jfree.plotter.listener;
 
 import hep.aida.IHistogram1D;
-import hep.aida.jfree.converter.Histogram1DConverter;
 
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.xy.XYDataset;
@@ -16,15 +15,5 @@ public class Histogram1DListener extends PlotListener<IHistogram1D> {
 
     Histogram1DListener(IHistogram1D histogram, JFreeChart chart, XYDataset dataset) {
         super(histogram, chart, dataset);
-    }
-    
-    @Override
-    public void update() {  
-        double xMinSize = this.plot.maxBinHeight() + Histogram1DConverter.findMaxError(plot);
-        if (xMinSize > 0.) {
-            this.chart.getXYPlot().getRangeAxis().setAutoRangeMinimumSize(xMinSize);
-            this.chart.getXYPlot().getRangeAxis().configure();
-        }
-        super.update();
     }
 }

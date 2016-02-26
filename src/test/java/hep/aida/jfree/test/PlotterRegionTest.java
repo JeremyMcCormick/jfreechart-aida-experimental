fb41b8c1d7553fba19475ec90dd5ed123ced6aaf
JeremyMcCormick <jermccormick@gmail.com>
2016-02-26 13:01:38 -0800

    Add plotter region test stub (not implemented yet).

---
+++ src/test/java/hep/aida/jfree/test/PlotterRegionTest.java
@@ -0,0 +1,86 @@
+package hep.aida.jfree.test;
+
+import junit.framework.TestCase;
+
+public class PlotterRegionTest extends TestCase {
+
+    // void applyStyle(IPlotterStyle style)
+    
+    // String[] availableParameterOptions(String parameter)
+     
+    // String[] availableParameters()
+     
+    // void clear()
+    
+    // IInfo info()
+    
+    // IPlotterLayout layout()
+    
+    // Stuff in IPlotterLayout....
+    // String[] availableParameters()
+    // double parameterValue(String paramName)
+    // void reset()
+    // boolean setParameter(String paramName, double paramValue)    
+    
+    // String parameterValue(String parameter)
+        
+    // void plot(IBaseHistogram histogram)
+
+    // void plot(IBaseHistogram histogram, IPlotterStyle style)
+     
+    // void plot(IBaseHistogram histogram, IPlotterStyle style, String options)     
+
+    // void plot(IBaseHistogram histogram, String options)
+    
+    // void plot(IDataPointSet dataPointSet)
+     
+    // void plot(IDataPointSet dataPointSet, IPlotterStyle style)
+     
+    // void plot(IDataPointSet dataPointSet, IPlotterStyle style, String options)
+     
+    // void plot(IDataPointSet dataPointSet, String options)
+     
+    // void plot(IFunction function)
+     
+    // void plot(IFunction function, IPlotterStyle style)
+     
+    // void plot(IFunction function, IPlotterStyle style, String options)
+     
+    // void plot(IFunction function, String options)
+     
+    // void remove(IBaseHistogram histogram)
+    
+    // void remove(IDataPointSet dataPointSet)
+     
+    // void remove(IFunction function)
+     
+    // void setLayout(IPlotterLayout layout)
+     
+    // void setParameter(String parameter)
+    
+    // void setParameter(String parameter, String options)
+    
+    // void setStyle(IPlotterStyle style)
+    
+    // void setTitle(String title)
+    
+    // void setXLimits()
+    
+    // void setXLimits(double min)
+    
+    // void setXLimits(double min, double max)
+    
+    // void setYLimits()
+     
+    // void setYLimits(double min)
+     
+    // void setYLimits(double min, double max)
+     
+    // void setZLimits()
+     
+    // void setZLimits(double min)
+     
+    // void setZLimits(double min, double max)
+     
+    // IPlotterStyle style()
+}
